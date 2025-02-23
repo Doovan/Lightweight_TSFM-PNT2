ec89048f77c969ef569beaff3f167f62455d91d1
volbbin.kong <volbbin-kong@163.com>
2025-02-23 15:23:59 +0800

    基础Pointnet模型-成功训练修改版

---
+++ Pointnet2_Transformer_pytorch/data_utils/ShapeNetDataLoader.py
@@ -0,0 +1,152 @@
+# *_*coding:utf-8 *_*
+import os
+import json
+import warnings
+import numpy as np
+from torch.utils.data import Dataset
+import glob
+import torch
+
+warnings.filterwarnings('ignore')
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    pc = pc / m
+    return pc
+
+class PartNormalDataset(Dataset):
+    def __init__(self,root = './data/shapenetcore_partanno_segmentation_benchmark_v0_normal', npoints=2500, split='train', class_choice=None, normal_channel=False):
+        self.npoints = npoints
+        self.root = root
+        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
+        self.cat = {}
+        self.normal_channel = normal_channel
+
+
+        with open(self.catfile, 'r') as f:
+            for line in f:
+                ls = line.strip().split()
+                self.cat[ls[0]] = ls[1]
+        self.cat = {k: v for k, v in self.cat.items()}
+        self.classes_original = dict(zip(self.cat, range(len(self.cat))))
+
+        if not class_choice is  None:
+            self.cat = {k:v for k,v in self.cat.items() if k in class_choice}
+        # print(self.cat)
+
+        self.meta = {}
+        with open(os.path.join(self.root, 'train_test_split', 'shuffled_train_file_list.json'), 'r') as f:
+            train_ids = set([str(d.split('/')[2]) for d in json.load(f)])
+        with open(os.path.join(self.root, 'train_test_split', 'shuffled_val_file_list.json'), 'r') as f:
+            val_ids = set([str(d.split('/')[2]) for d in json.load(f)])
+        with open(os.path.join(self.root, 'train_test_split', 'shuffled_test_file_list.json'), 'r') as f:
+            test_ids = set([str(d.split('/')[2]) for d in json.load(f)])
+        for item in self.cat:
+            # print('category', item)
+            self.meta[item] = []
+            dir_point = os.path.join(self.root, self.cat[item])
+            fns = sorted(os.listdir(dir_point))
+            # print(fns[0][0:-4])
+            if split == 'trainval':
+                fns = [fn for fn in fns if ((fn[0:-4] in train_ids) or (fn[0:-4] in val_ids))]
+            elif split == 'train':
+                fns = [fn for fn in fns if fn[0:-4] in train_ids]
+            elif split == 'val':
+                fns = [fn for fn in fns if fn[0:-4] in val_ids]
+            elif split == 'test':
+                fns = [fn for fn in fns if fn[0:-4] in test_ids]
+            else:
+                print('Unknown split: %s. Exiting..' % (split))
+                exit(-1)
+
+            # print(os.path.basename(fns))
+            for fn in fns:
+                token = (os.path.splitext(os.path.basename(fn))[0])
+                self.meta[item].append(os.path.join(dir_point, token + '.txt'))
+
+        self.datapath = []
+        for item in self.cat:
+            for fn in self.meta[item]:
+                self.datapath.append((item, fn))
+
+        self.classes = {}
+        for i in self.cat.keys():
+            self.classes[i] = self.classes_original[i]
+
+        # Mapping from category ('Chair') to a list of int [10,11,12,13] as segmentation labels
+        self.seg_classes = {'Earphone': [16, 17, 18], 'Motorbike': [30, 31, 32, 33, 34, 35], 'Rocket': [41, 42, 43],
+                            'Car': [8, 9, 10, 11], 'Laptop': [28, 29], 'Cap': [6, 7], 'Skateboard': [44, 45, 46],
+                            'Mug': [36, 37], 'Guitar': [19, 20, 21], 'Bag': [4, 5], 'Lamp': [24, 25, 26, 27],
+                            'Table': [47, 48, 49], 'Airplane': [0, 1, 2, 3], 'Pistol': [38, 39, 40],
+                            'Chair': [12, 13, 14, 15], 'Knife': [22, 23]}
+
+        # for cat in sorted(self.seg_classes.keys()):
+        #     print(cat, self.seg_classes[cat])
+
+        self.cache = {}  # from index to (point_set, cls, seg) tuple
+        self.cache_size = 20000
+
+
+    def __getitem__(self, index):
+        if index in self.cache:
+            point_set, cls, seg = self.cache[index]
+        else:
+            fn = self.datapath[index]
+            cat = self.datapath[index][0]
+            cls = self.classes[cat]
+            cls = np.array([cls]).astype(np.int32)
+            data = np.loadtxt(fn[1]).astype(np.float32)
+            if not self.normal_channel:
+                point_set = data[:, 0:3]
+            else:
+                point_set = data[:, 0:6]
+            seg = data[:, -1].astype(np.int32)
+            if len(self.cache) < self.cache_size:
+                self.cache[index] = (point_set, cls, seg)
+        point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+
+        choice = np.random.choice(len(seg), self.npoints, replace=True)
+        # resample
+        point_set = point_set[choice, :]
+        seg = seg[choice]
+
+        return point_set, cls, seg
+
+    def __len__(self):
+        return len(self.datapath)
+
+#加载个人数据集的类
+class DealMyDataset(Dataset):
+    def __init__(self, root="/home/kong-vb/data_set/label_data", sample_points=2500):
+        self.file_paths = glob.glob(os.path.join(root, "*.asc"))  # 点云文件路径
+        self.sample_points = sample_points  # 每次采样的点数
+        self.clsSum = {"useful", "unuseful"}  # 可用/不可用类标签
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        # 加载点云数据
+        point_cloud_data = np.loadtxt(self.file_paths[idx]).astype(np.float32)
+
+        total_points = len(point_cloud_data)  # 获取文件中总点数
+        #print(f"Total points in file {self.file_paths[idx]}: {total_points}")
+
+        # 如果点云数量少于 sample_points，则允许重复选择
+        if total_points < self.sample_points:
+            choice = np.random.choice(total_points, self.sample_points, replace=True)
+        else:
+            choice = np.random.choice(total_points, self.sample_points, replace=False)
+
+        # 选择点云数据的坐标和标签
+        point_set = point_cloud_data[choice, 0:3]  # 选择前三列作为 x, y, z 坐标
+        seg = point_cloud_data[choice, -1].astype(np.int32)  # 标签（分割任务中的类别）
+
+        # 假设你有一个不同的 `cls` 标签来表示整个点云的类别（比如整个点云是"建筑"或"车"等）
+        cls = np.array([0]).astype(np.int32)  # 这里可以根据需要更新，通常是整个点云的一个类别标签
+
+        # 返回数据（点云坐标，整个点云类别，分割标签）
+        return point_set, cls, seg
+
